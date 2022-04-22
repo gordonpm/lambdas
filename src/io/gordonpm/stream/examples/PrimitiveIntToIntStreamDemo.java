69ba39bd694a4062e67cdebda82aee489eca4bf9
gordonpm <gordonm0815@gmail.com>
2022-04-22 13:27:24 -0700

    renamed class

--- src/io/gordonpm/stream/examples/PrimitiveIntToIntStreamDemo.java
+++ src/io/gordonpm/stream/examples/PrimitiveIntToIntStreamDemo.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public class PrimitiveIntToStreamIntDemo {
+public class PrimitiveIntToIntStreamDemo {
     public static void main(String[] args) {
         int[][] intArray = {{1, 2, 3}, {4, 5, 6}};
         System.out.println(intArray[1][2]);
